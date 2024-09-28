4cacacfd762bdc5ea5b8ed54fdc2c9a389d4f623
malendeja2 <jmalendeja1985@gmail.com>
2024-09-28 12:51:48 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>MACHINGA GROUP PRODUCTS</title>
+</head>
+<body>
+
+<nav class="navbar">
+    <div class="logo"><h1>MACHINGA GROUP PRODUCTS</h1></div>
+    <ul class="menu">
+        <li><a href="index.php" class="active">Home</a></li>
+        <li><a href="login.php" >Products</a></li>
+        <li><a href="login.php">About</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href=""><i class="fas fa-shopping-cart"></i></a></li>
+    </ul>
+<!-- for responsive button -->
+    <div class="menu-btn">
+        <i class="fa fa-bars"></i>
+    </div>
+</nav>
+
+<section class="content">
+    <h1>New Products for Women</h1>
+    <p>We Offer You New Women's clothes And Ornaments</p>
+    <button><a href="login.php">Shop Now</a></button>
+</section>
+
+<h1 class="pheading">Featured Products</h1>
+
+<section class="sec">
+    <div class="products">
+     <!--start card-->
+     <div class="card">
+            <div class="img"><img src="13.jpg" alt=""></div>
+            <div class="desc">Women</div>
+            <div class="title">Lady</div>
+            <div class="box">
+                <div class="price">55,000/=</div>
+                <button class="btn"><li><a href="login.php">By Now</a></li></button>
+            </div>
+        </div>
+    <!--end card-->
+
+        <!--start card-->
+        <div class="card">
+            <div class="img"><img src="14.jpg" alt=""></div>
+            <div class="desc">Women</div>
+            <div class="title">Jewelly</div>
+            <div class="box">
+                <div class="price">20,000/=</div>
+                <button class="btn"><li><a href="login.php">By Now</a></li></button>
+            </div>
+        </div>
+    <!--end card-->
+
+        <!--start card-->
+        <div class="card">
+            <div class="img"><img src="12.jpg" alt=""></div>
+            <div class="desc">Women</div>
+            <div class="title">Gauni</div>
+            <div class="box">
+                <div class="price">35,000/=</div>
+                <button class="btn"><li><a href="login.php">By Now</a></li></button>
+            </div>
+        </div>
+    <!--end card-->
+
+        <!--start card-->
+        <div class="card">
+            <div class="img"><img src="23.jpg" alt=""></div>
+            <div class="desc">Women</div>
+            <div class="title">Dela</div>
+            <div class="box">
+                <div class="price">45,000/=</div>
+                <button class="btn"><li><a href="login.php">By Now</a></li></button>
+            </div>
+        </div>
+    <!-- end card-->
+
+    <!--start card-->
+    <div class="card">
+                <div class="img"><img src="17.jpg" alt=""></div>
+                <div class="desc">Women</div>
+                <div class="title">Gauni</div>
+                <div class="box">
+                    <div class="price">55,000/=</div>
+                    <button class="btn"><li><a href="login.php">By Now</a></li></button>
+                </div>
+            </div>
+        <!--end card-->
+    
+            <!--start card-->
+            <div class="card">
+                <div class="img"><img src="16.jpg" alt=""></div>
+                <div class="desc">Women</div>
+                <div class="title">Shoes</div>
+                <div class="box">
+                    <div class="price">40,000/=</div>
+                    <button class="btn"><li><a href="login.php">By Now</a></li></button>
+                </div>
+            </div>
+        <!--end card-->
+
+</div>
+</section>
+
+<h1 class="pheading">Latest Products</h1>
+<section class="sec">
+    <div class="products">
+            <!--start card-->
+            <div class="card">
+                <div class="img"><img src="22.jpg" alt=""></div>
+                <div class="desc">Women</div>
+                <div class="title">Trouza</div>
+                <div class="box">
+                    <div class="price">785,000/=</div>
+                    <button class="btn"><li><a href="login.php">By Now</a></li></button>
+                </div>
+            </div>
+        <!--end card-->
+    
+           
+            <!--start card-->
+            <div class="card">
+                <div class="img"><img src="18.jpg" alt=""></div>
+                <div class="desc">Women</div>
+                <div class="title">Gauni</div>
+                <div class="box">
+                    <div class="price">60,000/=</div>
+                    <button class="btn"><li><a href="login.php">By Now</a></li></button>
+                </div>
+            </div>
+        <!--end card-->
+    
+            <!--start card-->
+            <div class="card">
+                <div class="img"><img src="17.jpg" alt=""></div>
+                <div class="desc">Women</div>
+                <div class="title">Gauni</div>
+                <div class="box">
+                    <div class="price">55,000/=</div>
+                    <button class="btn"><li><a href="login.php">By Now</a></li></button>
+                </div>
+            </div>
+        <!--end card-->
+    
+          <!--start card-->
+        <div class="card">
+            <div class="img"><img src="19.jpg" alt=""></div>
+            <div class="desc">Women</div>
+            <div class="title">Open shoes</div>
+            <div class="box">
+                <div class="price">50,000/=</div>
+                <button class="btn"><li><a href="login.php">By Now</a></li></button>
+            </div>
+        </div>
+    <!--end card-->
+
+        <!--start card-->
+        <div class="card">
+            <div class="img"><img src="21.jpg" alt=""></div>
+            <div class="desc">Women</div>
+            <div class="title">Pafume</div>
+            <div class="box">
+                <div class="price">30,000/=</div>
+                <button class="btn"><li><a href="login.php">By Now</a></li></button>
+            </div>
+        </div>
+    <!--end card-->
+
+     <!--start card-->
+     <div class="card">
+            <div class="img"><img src="15.jpg" alt=""></div>
+            <div class="desc">Women</div>
+            <div class="title">Jewelly</div>
+            <div class="box">
+                <div class="price">30,000/=</div>
+                <button class="btn"><li><a href="login.php">By Now</a></li></button>
+            </div>
+        </div>
+    <!--end card-->
+    </div>
+</section>
+    <footer>
+        <p>Machinga group</p> <br>
+	<p> <a href="index.php">Home</a> | <a href="login.php">Products</a> | <a href="login.php">About</a></p>
+<!--jquery-->
+<script>
+    $(".menu-btn").click(function (){
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    })
+</script>
+</body>
+</html>
